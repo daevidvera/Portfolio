79d1bac002dbddf691078839b49648c055760162
Carlos David Rivera <carlosrescoto04@gmail.com>
2025-05-19 17:53:13 -0600

    changes to the porftolio played around the project cards a little bit'

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-/**
- * Scrolls the browser window to the element with the given
- * id attribute, setting its relative location to be on top.
- */
-function scrollToElement() {
-    // Find and remove the last "active" nav element, if exists
-    let lastActive = getLastActive();
+function scrollToElement(event) {
+    event.preventDefault(); 
+
+    let lastActive = document.querySelector('.navbar-nav .active');
     if (lastActive) {
         lastActive.classList.remove('active');
     }
 
-    window.event.target.classList.add('active');
-    let id = window.event.target.getAttribute('data-target')
+    if (event.currentTarget.classList.contains('nav-link')) {
+        event.currentTarget.classList.add('active');
+    }
+
+    
+    let id = event.currentTarget.getAttribute('data-target');
     let el = document.getElementById(id);
-    let nav = document.querySelector('nav');
-    let top = el.getBoundingClientRect().top + window.pageYOffset - nav.offsetHeight - 4;
 
-    window.scrollTo({
-        top: top,
-        behavior: 'smooth'
-    });
-}
-
-function getLastActive() {
-    return document.querySelector('.active');
+    if (el) {
+    
+        let nav = document.querySelector('nav');
+        let top = el.getBoundingClientRect().top + window.pageYOffset - nav.offsetHeight - 4;
+        window.scrollTo({
+            top: top,
+            behavior: 'smooth'
+        });
+    }
 }
