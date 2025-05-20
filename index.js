3dbab2068e878a4c3cff1fb1e2a6a526b8388065
Carlos David Rivera <carlosrescoto04@gmail.com>
2025-05-19 18:40:19 -0600

    fix a bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function scrollToElement(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     let lastActive = document.querySelector('.navbar-nav .active');
     if (lastActive) {
@@ -10,17 +10,47 @@ function scrollToElement(event) {
         event.currentTarget.classList.add('active');
     }
 
-    
     let id = event.currentTarget.getAttribute('data-target');
     let el = document.getElementById(id);
+    let nav = document.querySelector('nav');
 
     if (el) {
-    
-        let nav = document.querySelector('nav');
-        let top = el.getBoundingClientRect().top + window.pageYOffset - nav.offsetHeight - 4;
+        let topOffset = id === "home" ? 10 : nav.offsetHeight + 4;
+        let top = el.getBoundingClientRect().top + window.pageYOffset - topOffset;
+
         window.scrollTo({
             top: top,
             behavior: 'smooth'
         });
     }
+
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        new bootstrap.Collapse(navbarCollapse).toggle();
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hash = window.location.hash.substring(1);
+    const el = document.getElementById(hash);
+    const nav = document.querySelector('nav');
+
+    if (el) {
+        let topOffset = hash === "home" ? 10 : nav.offsetHeight + 4;
+        let top = el.getBoundingClientRect().top + window.pageYOffset - topOffset;
+
+        window.scrollTo({
+            top: top,
+            behavior: 'smooth'
+        });
+
+        const navLinks = document.querySelectorAll('.nav-link');
+        navLinks.forEach(link => {
+            if (link.getAttribute('data-target') === hash) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+});
